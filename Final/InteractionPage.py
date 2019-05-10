0051b5a09566043522ca43d460a97b62b1d21142
manish.apte <maapte@deloitte.com>
2019-05-10 14:26:32 +0530

    Refactoring Download and Interaction Pages

--- Final/InteractionPage.py
+++ Final/InteractionPage.py
@@ -22,6 +22,108 @@ class InteractionPage:
         self.user_auth_state = ""
         self.site_interaction_event = "true"
         self.interaction_name = ""
-        self.interaction_tool_name = ""
-        self.interactive_tool_complete = ""
-        self.interactive_tool_start = ""
+
+    @property
+    def get_page_name(self):
+        return self.page_name
+
+    def set_page_name(self, page_name):
+        self.page_name = page_name
+
+    @property
+    def get_page_referrer(self):
+        return self.page_referrer
+
+    def set_page_referrer(self, page_referrer):
+        self.page_referrer = page_referrer
+
+    @property
+    def get_page_language(self):
+        return self.page_language
+
+    def set_page_language(self, page_language):
+        self.page_language = page_language
+
+    @property
+    def get_page_view(self):
+        return self.page_view
+
+    def set_page_view(self, page_view):
+        self.page_view = page_view
+
+    @property
+    def get_page_accessibility(self):
+        return self.page_accessibility
+
+    def set_page_accessibility(self, page_accessibility):
+        self.page_accessibility = page_accessibility
+
+    @property
+    def get_page_path(self):
+        return self.page_path
+
+    def set_page_path(self, page_path):
+        self.page_path = page_path
+
+    @property
+    def get_page_url(self):
+        return self.page_url
+
+    def set_page_url(self, page_url):
+        self.page_url = page_url
+
+    @property
+    def get_page_login(self):
+        return self.page_login
+
+    def set_page_login(self, page_login):
+        self.page_login = page_login
+
+    @property
+    def get_page_hierarchy(self):
+        return self.page_hierarchy
+
+    def set_page_hierarchy(self, page_hierarchy):
+        self.page_hierarchy = page_hierarchy
+
+    @property
+    def get_login_event(self):
+        return self.login_event
+
+    def set_login_event(self, login_event):
+        self.login_event = login_event
+
+    @property
+    def get_user_id(self):
+        return self.user_id
+
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+
+    @property
+    def get_user_auth_state(self):
+        return self.user_auth_state
+
+    def set_user_auth_state(self, user_auth_state):
+        self.user_auth_state = user_auth_state
+
+    @property
+    def get_user_type(self):
+        return self.user_type
+
+    def set_user_type(self, user_type):
+        self.user_type = user_type
+
+    @property
+    def get_site_interaction_event(self):
+        return self.site_interaction_event
+
+    def set_site_interaction_event(self,site_interaction_event):
+        self.site_interaction_event = site_interaction_event
+
+    @property
+    def get_interaction_name(self):
+        return self.interaction_name;
+
+    def set_interaction_name(self,interaction_name):
+        self.interaction_name = interaction_name
